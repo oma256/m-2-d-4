fb6bc16b3e80fe7b0456afdcc8b38762591d2e85
Omurbek Dulatov <oma.dulatov@gmail.com>
2024-03-25 20:07:28 +0600

    done task 2

--- main.py
+++ main.py
@@ -6,27 +6,52 @@
 три раза, число 3 - два раза, числа 2 и 4 - по одному разу.
 """
 
-class MyList:
+# class MyList:
 
-    def __init__(self, num_list) -> None:
-        self.num_list = num_list
+#     def __init__(self, num_list) -> None:
+#         self.num_list = num_list
     
-    def get_num_rep_count(self):
-        result = {}
+#     def get_num_rep_count(self):
+#         result = {}
 
-        for num in self.num_list:
-            if num in result:
-                result[num] += 1
-            else:
-                result[num] = 1
+#         for num in self.num_list:
+#             if num in result:
+#                 result[num] += 1
+#             else:
+#                 result[num] = 1
 
         
-        for k, v in result.items():
-            print(f'Число {k} встречается {v}')
+#         for k, v in result.items():
+#             print(f'Число {k} встречается {v}')
 
-    def __str__(self) -> str:
-        return f'Объект список: {self.num_list}'
+#     def __str__(self) -> str:
+#         return f'Объект список: {self.num_list}'
 
-my_list = MyList([1, 1, 3, 2, 1, 3, 4])
-my_list.get_num_rep_count()
-print(my_list)
+# my_list = MyList([1, 1, 3, 2, 1, 3, 4])
+# my_list.get_num_rep_count()
+# print(my_list)
+
+
+"""
+2) Задача
+В строке заменить пробелы символом *
+В строке заменить пробелы звездочкой. Если встречается подряд несколько 
+пробелов, то их следует заменить одним знаком "*", пробелы в начале и конце 
+строки удалить.
+"""
+
+class Star:
+
+    def __init__(self, text) -> None:
+        self.text = text
+    
+    def replace_space_to_star(self):
+        result = ''
+        for t in self.text.strip().split():
+            result += t
+            result += '*'
+
+        print(result[:-1])
+
+star = Star('   Hello  Wor   ld!   ')
+star.replace_space_to_star()
