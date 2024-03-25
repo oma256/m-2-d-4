614163288fe737d7337ac93ac6d2d6bcd9fd8462
Omurbek Dulatov <oma.dulatov@gmail.com>
2024-03-25 20:58:07 +0600

    task 4 done

--- main.py
+++ main.py
@@ -40,18 +40,64 @@
 строки удалить.
 """
 
-class Star:
+# class Star:
 
-    def __init__(self, text) -> None:
-        self.text = text
+#     def __init__(self, text) -> None:
+#         self.text = text
     
-    def replace_space_to_star(self):
-        result = ''
-        for t in self.text.strip().split():
-            result += t
-            result += '*'
+#     def replace_space_to_star(self):
+#         result = ''
+#         for t in self.text.strip().split():
+#             result += t
+#             result += '*'
 
-        print(result[:-1])
+#         print(result[:-1])
 
-star = Star('   Hello  Wor   ld!   ')
-star.replace_space_to_star()
+# star = Star('   Hello  Wor   ld!   ')
+# star.replace_space_to_star()
+
+
+"""
+3) Задача
+Напишите программу, которая принимает имя файла и выводит его расширение. 
+Если расширение у файла определить невозможно, выбросите исключение.
+"""
+
+# import os
+
+
+# class Path:
+#     def __init__(self, path) -> None:
+#         self.path = path
+
+#     def get_files_ext(self):
+#         for name in os.listdir(path=self.path):
+#             if os.path.isfile(os.path.join(self.path, name)):
+#                 if name.split('.')[-1] in ('pdf', 'txt', 'epub'):
+#                     print(f"{name.split('.')[0]} - " 
+#                           f"расширение {name.split('.')[-1]}")
+
+
+# path = Path('/Users/oma/Desktop')
+# path.get_files_ext()
+
+
+"""
+Задача 4
+Сложите цифры целого числа 35732
+"""
+
+class Number:
+    def __init__(self, number) -> None:
+        self.number = number
+
+    def get_num_sum(self):
+        result = 0
+        for n in str(self.number):
+            result += int(n)
+        
+        return result
+
+
+number = Number(number=35732)
+print(number.get_num_sum())
